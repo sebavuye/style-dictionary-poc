b1b4fa1a6517730ed82b1a3524b126fc1959199a
Sebastian Vuye <sebastian.vuye@gmail.com>
2022-09-16 12:41:43 +0200

    feat(project setup): add style dictionary setup

---
+++ src/consts/variables.const.ts
@@ -0,0 +1,21 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 16 Sep 2022 10:38:39 GMT
+ */
+
+export const COLOR_BASE_GRAY_1 = "#ecf2f6";
+export const COLOR_BASE_GRAY_2 = "#d9e5f2";
+export const COLOR_BASE_GRAY_3 = "#b6c7da";
+export const COLOR_BASE_GRAY_4 = "809dbe";
+export const COLOR_BASE_GRAY_2_HALF_OPACITY = "#d9e5f2";
+export const COLOR_BASE_WHITE = "#ffffff";
+export const COLOR_BASE_BLACK = "#000000";
+export const FONT_SIZE_SMALL = "12px";
+export const FONT_SIZE_MEDIUM = "18px";
+export const FONT_SIZE_LARGE = "24px";
+export const SPACING_EXTRA_SMALL = "4px";
+export const SPACING_SMALL = "8px";
+export const SPACING_NORMAL = "16px";
+export const SPACING_MEDIUM = "24px";
+export const SPACING_LARGE = "32px";
+export const SPACING_EXTRA_LARGE = "40px";
